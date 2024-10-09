5b35cc2cf9b816ef1b043329589596860f7c0503
Anusha156 <anushareddyboru156@gmail.com>
2024-10-09 17:12:43 +0530

    admin

---
+++ adminservice/src/main/java/com/adminservice/controller/AdminControllerAdvice.java
@@ -0,0 +1,8 @@
+package com.adminservice.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class AdminControllerAdvice { // using global exception we can do this
+
+}
---
+++ adminservice/src/test/resources/AdminServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.adminservice
+org.junit.jupiter.api.Test;
+
+public class AdminServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
